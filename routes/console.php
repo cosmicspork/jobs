6c66b5fc967657a7b0f75367861bbf590ae96dd9
cosmicspork <106683649+cosmicspork@users.noreply.github.com>
2026-04-14 20:28:30 -0500

    fix: add timezone to emails

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('jobs:scrape')->hourly();
-Schedule::command('digest:send')->dailyAt('08:00');
+Schedule::command('digest:send')->dailyAt('08:00')->timezone('America/Chicago');
